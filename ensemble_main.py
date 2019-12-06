1e6ef48d22952af00e3de22df26a86f1dd89d778
Yingkai Sha <yingkai@eoas.ubc.ca>
2019-12-06 16:21:51 -0700

    bug fix on the 'left-closed thres intervals'. Adding documentaions

--- ensemble_main.py
+++ ensemble_main.py
@@ -15,12 +15,15 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 def dummy_module(delta_day, day0):
+    '''
+    dummy module for opertional test
+    '''
     date_ref = datetime.utcnow()+relativedelta(days=delta_day)
     print('The module is happy running '+date_ref.strftime('%Y%m%d'))
     return date_ref.day
 
 def main(delta_day, day0, key):
-    # variable "key" used two times # <-----
+    # extracting namelist content
     if key == 20:
         fcst_keys = fcst_keys_20Z
         tssc_keys = tssc_keys_20Z
@@ -33,6 +36,7 @@ def main(delta_day, day0, key):
         ENS_path  = ENS_path_08Z
         OTS_path  = OTS_path_08Z
         output_name = output_name_08Z
+    # UTC time corrections & filename creation
     date_ref = datetime.utcnow()+relativedelta(days=delta_day)
     date_ref_delay = date_ref-relativedelta(days=1) # use yesterday's forecast
     date_BJ = date_ref_delay+relativedelta(hours=8)
@@ -40,14 +44,15 @@ def main(delta_day, day0, key):
     name_today = []
     name_today.append(datetime.strftime(date_ref_delay, ENS_path))
     name_today.append(datetime.strftime(date_BJ, OTS_path))
+    # =========== import gridded data =========== #
     print('Import all micaps files')
     lon, lat = mt.genrate_grid()
-    # Initializing dictionaries
+    ## Initializing dictionaries
     cmpt_keys = ['ENS', 'OTS']
     dict_var = {}; dict_interp = {}; dict_header = {}
     dict_var = ini_dicts(dict_var, cmpt_keys)
     dict_interp = ini_dicts(dict_interp, cmpt_keys)
-    # Fill dictionaries with data    
+    ## Fill dictionaries with data    
     for fcst_key in fcst_keys:
         for i, name in enumerate(name_today):
             temp_name = name+fcst_key
@@ -70,27 +75,39 @@ def main(delta_day, day0, key):
     print('Bilinear interpolation for grid transfer')
     for key, val in dict_var.items():
         for fcst_key in fcst_keys:
-            #print(key)
-            #print(fcst_key)
-            #print(val[fcst_key].shape)
-            dict_interp[key][fcst_key] = mt.grid_transfer(dict_latlon[key][fcst_key][0], dict_latlon[key][fcst_key][1], val[fcst_key], lon, lat)
-    # Caliberating weights
+            dict_interp[key][fcst_key] = mt.grid_transfer(dict_latlon[key][fcst_key][0], 
+                                                          dict_latlon[key][fcst_key][1], 
+                                                          val[fcst_key], lon, lat)
+    # =========================================== #
+    # ========== Ensemble Caliberation ========== #
+    ## calibrating rules:
+    ##     0-25 mm - OTS only
+    ##     25-50, 50-inf - ENS, OTS weighted average
+    ##                     out = [a*ENS + b*OTS]/(a+b)
     print('Caliberating weights')
+    # ---------- Extracting weights ---------- #
+    # initialization
+    # W[precip ranges][fcst horizon][source, e.g., OTS]
     W = {}; W = ini_dicts(W, prec_keys)
     for prec_key in prec_keys:
         W[prec_key] = ini_dicts(W[prec_key], tssc_keys)
-    # Get weights from moving averaging
+    # TS weights + moving average
     for prec_key in prec_keys:
         for tssc_key in tssc_keys:
             # retreive ts files by the fcst delay
-            date_temp = date_ref - relativedelta(days=int(tssc_key)/24)
-            data_ma, flag_TS = et.norm_ensemble(date_temp.strftime(TS_perfix), 10, TS_path+prec_key+'/', period=tssc_key)
+            date_temp = date_ref - relativedelta(days=int(tssc_key)/24) # from fcst horizon (i.e., hours) to days
+            data_ma, flag_TS = et.norm_ensemble(date_temp.strftime(TS_perfix), 
+                                                10, TS_path+prec_key+'/', period=tssc_key) # <---- moving window size: 10
+            # saving weights to the dictionary
+            # case: no TS files
             if np.logical_not(flag_TS):
                 return day0 # <--- exit if no TS files
+            # case: TS filled with NaNs (vals = 9999.0)
             elif np.isnan(data_ma.as_matrix()[-1, 1:].astype(np.float)).sum() >= 3:
                 print('Warning: TS filled with NaNs, use average.')
                 for cmpt_key in cmpt_keys:
                     W[prec_key][tssc_key][cmpt_key] = 0.5
+            # case: regular (good quality) weights
             else:
                 for cmpt_key in cmpt_keys:
                     temp = data_ma[cmpt_key][9:10].as_matrix()
@@ -103,24 +120,26 @@ def main(delta_day, day0, key):
     # get all the result at the current day
     print('Total: '+str(len(fcst_keys)))
     for i in range(len(fcst_keys)):
-        # initialization
+        # initialization (three sets of weights: [0, 25, 50])
         W0 = 0.0
         W25 = 0.0
         W50 = 0.0
         precip0  = np.zeros(lon.shape)
         precip25 = np.zeros(lon.shape)
         precip50 = np.zeros(lon.shape)
-        #
         for cmpt_key in cmpt_keys:
             data0, data25, data50 = subtrack_precip_lev(dict_interp[cmpt_key][fcst_keys[i]])
-            precip0 += data0
-            precip25 += W['25'][tssc_keys[i]][cmpt_key] * data25
+            if cmpt_key = 'OTS':
+                precip0 += data0
+                W0 += 1.0
+            # precip. with multiplicative weights    
+            precip25 += W['25'][tssc_keys[i]][cmpt_key] * data25 
             precip50 += W['50'][tssc_keys[i]][cmpt_key] * data50
-            W0 += 1.0
             W25 += W['25'][tssc_keys[i]][cmpt_key]
             W50 += W['50'][tssc_keys[i]][cmpt_key]
         print('Calculating '+fcst_keys[i])
         output[fcst_keys[i]] = precip0/W0 + precip25/W25 + precip50/W50
+        # =========================================== #
     # Preparing MICAPS file output
     for fcst_key in fcst_keys:
         metadata = mt.micaps_change_header(lon.shape, dict_header[fcst_key])
@@ -130,5 +149,5 @@ def main(delta_day, day0, key):
 
 day_out = main(int(argv[1]), int(argv[2]), int(argv[3]))
 with open('shaG_history.log', 'w') as fp:
-    fp.write(str(day_out).zfill(2))
+    fp.write(str(day_out).zfill(2)) # exporting the day of completion (and where to restart if fails)
 
--- namelist.py
+++ namelist.py
@@ -5,11 +5,13 @@ resx      = 0.5        # output longitude grid spacing
 latlim    = [1, 59]    # the latitude range of the domain
 lonlim    = [71, 139]  # the longitude range of the domain
 # =========== Ensemble forecast keys ============ #
-prec_keys = ['25', '50']                 # Keys of precipitation ranges
-fcst_keys_20Z = ['036', '060', '084', '108', '132', '156', '180', '204', '228' ,'240'] # Keys of objective analysis 
-fcst_keys_08Z = ['024', '048', '072', '096', '120', '144', '168', '192', '216', '240'] # Keys of objective analysis
-tssc_keys_20Z = ['024', '048', '072', '096', '120', '144', '168', '192', '216', '240'] # Keys of forecast scores
-tssc_keys_08Z = ['024', '048', '072', '096', '120', '144', '168', '192', '216', '240'] # Keys of forecast scores
+prec_keys = ['25', '50'] # Keys of precipitation ranges, < 25 mm is not weighted.
+ # Keys of objective analysis (i.e., gridded fiels) 
+fcst_keys_20Z = ['036', '060', '084', '108', '132', '156', '180', '204', '228' ,'240']
+fcst_keys_08Z = ['024', '048', '072', '096', '120', '144', '168', '192', '216', '240']
+# Keys of forecast scores (i.e., weights)
+tssc_keys_20Z = ['024', '048', '072', '096', '120', '144', '168', '192', '216', '240']
+tssc_keys_08Z = ['024', '048', '072', '096', '120', '144', '168', '192', '216', '240']
 # ================== File path ================== #
 TS_perfix = '%y%m%d08' # The perfix of TS
 TS_path   = 'U:/' #'data/25/17011808.096'
