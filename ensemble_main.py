db02f9e2a77a7c394641ded6c3050a0e3373af31
Yingkai Sha <yingkaisha@gmail.com>
2021-08-14 14:42:45 -0600

    update main

--- ensemble_main.py
+++ ensemble_main.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # import keywords from the namelist
-from namelist import lib_path, prec_keys, prec_keys_SE, prec_keys_SS, TS_perfix, TS_path, \
-                     tag_name, lonlim, latlim, lonlim_SE, latlim_SE, lonlim_SS, latlim_SS,\
-                     fcst_keys_08Z, tssc_keys_08Z, ENS_path_08Z, OTS_path_08Z, output_name_08Z, \
-                     fcst_keys_20Z, tssc_keys_20Z, ENS_path_20Z, OTS_path_20Z, output_name_20Z, flag_ens
+from namelist import lib_path, prec_keys, flag_SE, flag_SS, prec_keys_SE, prec_keys_SS, 
+                    TS_perfix, TS_path, tag_name, lonlim, latlim, lonlim_SE, latlim_SE, lonlim_SS, latlim_SS,\
+                     fcst_keys_08Z, tssc_keys_08Z, ENS_path_08Z, OTS_path_08Z, output_name_08Z, output_name_SE_08Z, output_name_SS_08Z\
+                     fcst_keys_20Z, tssc_keys_20Z, ENS_path_20Z, OTS_path_20Z, output_name_20Z, output_name_SE_20Z, output_name_SE_20Z, flag_ens
 from sys import path, argv
 path.insert(0, lib_path)
 
@@ -43,14 +43,19 @@ def main(delta_day, day0, key, flag_ens=flag_ens):
         tssc_keys = tssc_keys_20Z
         OTS_path  = OTS_path_20Z
         output_name = output_name_20Z
+        output_name_SE = output_name_SE_20Z
+        output_name_SS = output_name_SS_20Z
+        
         if flag_ens:
             ENS_path  = ENS_path_20Z
     else:
         fcst_keys = fcst_keys_08Z
         tssc_keys = tssc_keys_08Z
-        
         OTS_path  = OTS_path_08Z
         output_name = output_name_08Z
+        output_name_SE = output_name_SE_08Z
+        output_name_SS = output_name_SS_08Z
+        
         if flag_ens:
             ENS_path  = ENS_path_08Z
             
@@ -69,11 +74,25 @@ def main(delta_day, day0, key, flag_ens=flag_ens):
     print('Import all micaps files')
     
     lon, lat = mt.genrate_grid(lonlim=lonlim, latlim=latlim)
-    # Jiang-nan subsets
-    ind_SE, ind_SE = mt.grid_search(lon, lat, lonlim_SE, latlim_SE)
-    # Hua-nan subsets
-    ind_SE, ind_SE = mt.grid_search(lon, lat, lonlim_SS, latlim_SS)
 
+    if flag_SE:
+        prec_keys += prec_keys_SE
+        flag_heavy = True
+        # Jiang-nan subsets
+        indx_SE, indy_SE = mt.grid_search(lon, lat, lonlim_SE, latlim_SE)
+        lon_SE = lon[indx_SE[0]:indx_SE[1]+1, indy_SE[0]:indy_SE[1]+1]
+        lat_SE = lat[indx_SE[0]:indx_SE[1]+1, indy_SE[0]:indy_SE[1]+1]
+        grid_shape_SE = lon_SE.shape
+        
+    if flag_SS:
+        prec_keys += prec_keys_SS
+        flag_heavy = True
+        # Hua-nan subsets
+        indx_SS, indy_SS = mt.grid_search(lon, lat, lonlim_SS, latlim_SS)
+        lon_SS = lon[indx_SS[0]:indx_SS[1]+1, indy_SS[0]:indy_SS[1]+1]
+        lat_SS = lat[indx_SS[0]:indx_SS[1]+1, indy_SS[0]:indy_SS[1]+1]
+        grid_shape_SS = lon_SS.shape
+    
     ## Initializing dictionaries
     if flag_ens:
         cmpt_keys = ['ENS', 'OTS']
@@ -128,6 +147,7 @@ def main(delta_day, day0, key, flag_ens=flag_ens):
     # W[precip ranges][fcst horizon][source, e.g., OTS]
     
     print('Estimating calibration weights')
+    
     W = {}; W = ini_dicts(W, prec_keys)
     
     for prec_key in prec_keys:
@@ -141,32 +161,33 @@ def main(delta_day, day0, key, flag_ens=flag_ens):
             date_temp = date_ref - relativedelta(days=int(tssc_key)/24) # from fcst horizon (i.e., hours) to days
             
             # reading TS from selected files + TS moving average
-            data_ma, flag_TS = et.norm_ensemble(date_temp.strftime(TS_perfix), 
-                                                10, TS_path+prec_key+'/', period=tssc_key) # <---- moving window size: 10
-            
-            # TODO: testing data_ma shape --> add W['70'][..][..] and W['77'][...][..]
-            
+            ## The moving averaging considers 10 days backward 
+            data_ma, flag_TS = et.norm_ensemble(date_temp.strftime(TS_perfix), 10, TS_path+prec_key+'/', lead=tssc_key)
             
             # saving weights to the dictionary
-            # case: no TS files
+            # case: no TS files (flag_TS=False)
             if np.logical_not(flag_TS):
                 return day0 # <--- exit if no TS files
             
             # case: TS filled with NaNs (vals = 9999.0)
-            elif np.isnan(data_ma.as_matrix()[-1, 1:].astype(np.float)).sum() >= 3:
+            ## Use TS=0.5
+            elif np.isnan(data_ma.values[-1, 1:].astype(np.float)).sum() >= 3:
                 print('Warning: TS filled with NaNs, use average.')
                 for cmpt_key in cmpt_keys:
                     W[prec_key][tssc_key][cmpt_key] = 0.5
+                flag_heavy = False
                     
             # case: regular (good quality) weights
             else:
                 for cmpt_key in cmpt_keys:
-                    temp = data_ma[cmpt_key][9:10].as_matrix()
+                    temp = data_ma[cmpt_key][9:10].values
                     if np.isnan(temp):
                         temp = 0.0
                     W[prec_key][tssc_key][cmpt_key] = temp
                     
     # ---------- Extracting historical weights ---------- #
+    # Obtaining TS from last year the same initialization date
+    
     print('Caliberating historical weights')
     W_h = {}; W_h = ini_dicts(W_h, prec_keys)
     
@@ -180,50 +201,119 @@ def main(delta_day, day0, key, flag_ens=flag_ens):
             # the case of last year
             hist_ref = date_ref - relativedelta(years=1)
             date_temp = hist_ref + relativedelta(days=int(tssc_key)/24)
-            data_ma, flag_TS = et.norm_ensemble(date_temp.strftime(TS_perfix), 
-                                                10, TS_path+prec_key+'/', period=tssc_key, backward=False)
+            
+            # backward=False for historical TS
+            data_ma, flag_TS = et.norm_ensemble(date_temp.strftime(TS_perfix), 10, TS_path+prec_key+'/', lead=tssc_key, backward=False)
+            
             # saving weights to the dictionary
-            # case: no TS files
-            if (np.logical_not(flag_TS)) or (np.isnan(data_ma.as_matrix()[-1, 1:].astype(np.float)).sum() >= 3):
+            # case: no TS files (skip)
+            if (np.logical_not(flag_TS)) or (np.isnan(data_ma.values[-1, 1:].astype(np.float)).sum() >= 3):
                 print('Warning: historical TS no found/filled with NaNs. Now ignoring')
+                
                 for cmpt_key in cmpt_keys:
                     W_h[prec_key][tssc_key][cmpt_key] = W[prec_key][tssc_key][cmpt_key]
+                    
             # case: regular (good quality) weights
             else:
                 for cmpt_key in cmpt_keys:
-                    temp = data_ma[cmpt_key][9:10].as_matrix()
+                    temp = data_ma[cmpt_key][9:10].values
                     if np.isnan(temp):
                         temp = 0.0
                     W_h[prec_key][tssc_key][cmpt_key] = temp
+                    
     # Calculate ensembles
     print('Preparing output')
     output = {}
+    output_SE = {}
+    output_SS = {}
     
     # get all the result at the current day
     print('Total: '+str(len(fcst_keys)))
+    
     for i in range(len(fcst_keys)):
+        
+        # --------------------------------------------------- #
+        # 0, 25, 50 mm cases
         # initialization (three sets of weights: [0, 25, 50])
-        W0 = 0.0
-        W25 = 0.0
-        W50 = 0.0
+        W0 = 0.0; W25 = 0.0; W50 = 0.0
         precip0  = np.zeros(lon.shape)
         precip25 = np.zeros(lon.shape)
         precip50 = np.zeros(lon.shape)
         
         for cmpt_key in cmpt_keys:
+                        
             data0, data25, data50 = subtrack_precip_lev(dict_interp[cmpt_key][fcst_keys[i]])
+            
             if cmpt_key == 'OTS':
                 precip0 += data0
                 W0 += 1.0
                 
             # precip. with multiplicative weights    
-            precip25 += W['25'][tssc_keys[i]][cmpt_key] * data25 + W_h['25'][tssc_keys[i]][cmpt_key] * data25 
-            precip50 += W['50'][tssc_keys[i]][cmpt_key] * data50 + W_h['50'][tssc_keys[i]][cmpt_key] * data50
-            W25 += W['25'][tssc_keys[i]][cmpt_key] + W_h['25'][tssc_keys[i]][cmpt_key]
-            W50 += W['50'][tssc_keys[i]][cmpt_key] + W_h['50'][tssc_keys[i]][cmpt_key]
+            precip25 += W['25'][tssc_keys[i]][cmpt_key] * data25 # + W_h['25'][tssc_keys[i]][cmpt_key] * data25 
+            precip50 += W['50'][tssc_keys[i]][cmpt_key] * data50 # + W_h['50'][tssc_keys[i]][cmpt_key] * data50
+            W25 += W['25'][tssc_keys[i]][cmpt_key] # + W_h['25'][tssc_keys[i]][cmpt_key]
+            W50 += W['50'][tssc_keys[i]][cmpt_key] # + W_h['50'][tssc_keys[i]][cmpt_key]
             
         print('Calculating '+fcst_keys[i])
         output[fcst_keys[i]] = precip0/W0 + precip25/W25 + precip50/W50
+        
+        if flag_SE:
+            
+            W0 = 0.0; W25 = 0.0; W50 = 0.0; W_heavy = 0.0
+            precip0_SE  = np.zeros(grid_shape)
+            precip25_SE = np.zeros(grid_shape)
+            precip50_SE = np.zeros(grid_shape)
+            precip_heavy_SE = np.zeros(grid_shape)
+            
+            for cmpt_key in cmpt_keys:
+                
+                temp_SE = dict_interp[cmpt_key][fcst_keys[i]][indx_SE[0]:indx_SE[1]+1, indy_SE[0]:indy_SE[1]+1]
+                data0, data25, data50, data_heavy = subtrack_precip_lev(temp_SE, np.float(prec_keys_SE[0]))
+                
+                if cmpt_key == 'OTS':
+                    precip0 += data0
+                    W0 += 1.0
+                    
+                # precip. with multiplicative weights    
+                precip25_SE += W['25'][tssc_keys[i]][cmpt_key] * data25
+                precip50_SE += W['50'][tssc_keys[i]][cmpt_key] * data50
+                precip_heavy_SE += W[prec_keys_SE[0]][tssc_keys[i]][cmpt_key] * data_heavy
+                
+                W25 += W['25'][tssc_keys[i]][cmpt_key]
+                W50 += W['50'][tssc_keys[i]][cmpt_key]
+                W_heavy += W[prec_keys_SE[0]][tssc_keys[i]][cmpt_key]
+                
+        print('Calculating {} Jiang-nan'.format(fcst_keys[i]))
+        output_SE[fcst_keys[i]] = precip0/W0 + precip25/W25 + precip50/W50 + precip_heavy_SE/W_heavy
+
+        if flag_SS:
+            
+            W0 = 0.0; W25 = 0.0; W50 = 0.0; W_heavy = 0.0
+            precip0_SS  = np.zeros(grid_shape)
+            precip25_SS = np.zeros(grid_shape)
+            precip50_SS = np.zeros(grid_shape)
+            precip_heavy_SS = np.zeros(grid_shape)
+            
+            for cmpt_key in cmpt_keys:
+                
+                temp_SS = dict_interp[cmpt_key][fcst_keys[i]][indx_SS[0]:indx_SS[1]+1, indy_SS[0]:indy_SS[1]+1]
+                data0, data25, data50, data_heavy = subtrack_precip_lev(temp_SS, np.float(prec_keys_SS[0]))
+                
+                if cmpt_key == 'OTS':
+                    precip0 += data0
+                    W0 += 1.0
+                    
+                # precip. with multiplicative weights    
+                precip25_SS += W['25'][tssc_keys[i]][cmpt_key] * data25
+                precip50_SS += W['50'][tssc_keys[i]][cmpt_key] * data50
+                precip_heavy_SS += W[prec_keys_SS0]][tssc_keys[i]][cmpt_key] * data_heavy
+                
+                W25 += W['25'][tssc_keys[i]][cmpt_key]
+                W50 += W['50'][tssc_keys[i]][cmpt_key]
+                W_heavy += W[prec_keys_SS0]][tssc_keys[i]][cmpt_key]
+                
+        print('Calculating {} Hua-nan'.format(fcst_keys[i]))
+        output_SS[fcst_keys[i]] = precip0/W0 + precip25/W25 + precip50/W50 + precip_heavy_SS/W_heavy
         # =========================================== #
         
     # Preparing MICAPS file output
@@ -231,6 +321,14 @@ def main(delta_day, day0, key, flag_ens=flag_ens):
         metadata = mt.micaps_change_header(lon.shape, dict_header[fcst_key])
         mt.micaps_export(datetime.strftime(date_BJ, output_name)+fcst_key+'.txt', metadata, output[fcst_key])
         
+        if flag_SE:
+            metadata_SE = mt.micaps_change_header(lon_SE.shape, dict_header[fcst_key])
+            mt.micaps_export(datetime.strftime(date_BJ, output_name_SE)+fcst_key+'.txt', metadata_SE, output_SE[fcst_key])
+            
+        if flag_SS:
+            metadata_SS = mt.micaps_change_header(lon_SS.shape, dict_header[fcst_key])
+            mt.micaps_export(datetime.strftime(date_BJ, output_name_SS)+fcst_key+'.txt', metadata_SS, output_SS[fcst_key])
+                             
     print('Ensemble post-processing complete')
     
     return date_ref.day
