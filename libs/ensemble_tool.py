22b0a788f8cc7ddcf192d7e5c69e8b538bbc946a
Yingkai Sha <yingkaisha@gmail.com>
2021-08-14 14:42:31 -0600

    update libs

--- libs/ensemble_tool.py
+++ libs/ensemble_tool.py
@@ -4,51 +4,109 @@ from glob import glob
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-def norm_ensemble(date, num, data_path, period='096', backward=True):
+def norm_ensemble(date, num, data_path, lead='96', backward=True):
     '''
-    Moving average and normalize TS files
+    Moving average and normalize TS files.
+    -----------------------------------------------------
+    The function is operated in the following order:
+        1. Identify a range of initialization times
+        2. Import TS
+        3. TS value cleaning
+        4. Moving window averaging and normalization
+        
+    -----------------------------------------------------
+    Input
+        date: the currently forecast initialization date
+        num: number of neighbourred days considered
+        data_path: the file path of ALL the TS files
+        lead: forecast lead time as hours and in string fmt.
+        backward: "num" days backward; False for forward (historical runs only)
+        
+    Output
+        data_ma: a pandas dataframe that contains all the TS; 
+                 it has four columns of 'Date', 'lead', 'OTS', 'ENS', 'FRE'.
+        flag: True for success; False for no files found.
     '''
     
     print(' - Checking TS at '+date)
+    
     base = datetime.strptime(date, '%y%m%d%H') # <------------- The starting date of files
+    
     if backward:
-        date_list = [base - relativedelta(days=x) for x in range(num)] # <-- how many days
+        # <-- how many days backward
+        date_list = [base - relativedelta(days=x) for x in range(num)] 
     else:
-        date_list = [base + relativedelta(days=x) for x in range(num)] # <-- how many days
-    # create empty dataframe
+        # <-- how many days forward (historical re-run only)
+        date_list = [base + relativedelta(days=x) for x in range(num)] 
+        
+    # The output dataframe
     data = pd.DataFrame()
     data = data.reindex(columns=['Date'])
     data['Date'] = date_list[::-1]
+    
     # loop over raw files
-    names = glob(data_path+'*'+date[-2:]+'.'+period)
+    # Example filename: 21072108.96 for 2021-07-21 08Z 96H forecast lead time
+    # the line below searches multi-day files by matching: data_path/*08.96
+    names = glob(data_path+'*'+date[-2:]+'.'+lead)
+    
+    # Handle no file cases
     if len(names) == 0:
         print('No files found in '+data_path+'. Exit ...')
         return 0, False
+    
+    # The dataframe that contains all the valid TS
     data_local = pd.DataFrame()
     for filename in names:
+        
+        # temp_data: the dataframe of a single file/lead time
+        # encoding='windows-1252' for windows
         temp_data = pd.read_csv(filename, delim_whitespace=True, encoding='windows-1252')
+        
+        # Exceping at least five columns for non-empty files
+        ## Skip this file is columns miss matched
         if len(temp_data.columns) < 5:
             continue
+        
+        ## Import TS
+        ## File head sequence: Date, Lead, tsRR, tsEc, tsT639, tsNcep, tsJap, tsOts, tsEns, tsFre, etc.
+        ## <----- !!! Note: The file head must have the order above
         else:
-            #print(temp_data)
+            # Discard column 2-7 (tsRR-tsJap)
             temp_data = temp_data.drop(temp_data.columns[2:7], axis=1)
+            # Discard anything after tsFre
             temp_data = temp_data.drop(temp_data.columns[5:], axis=1)
-            #print(temp_data)
-            temp_data.columns = ['Date', 'period', 'OTS', 'ENS', 'FRE']
+            
+            # Rename the pandas dataframe
+            temp_data.columns = ['Date', 'lead', 'OTS', 'ENS', 'FRE']
+            
+            # Convert str date to datetime format
             temp_data['Date'] = datetime.strptime(temp_data['Date'][0].astype(str), '%Y%m%d%H')
+            
+            # append the current lead time to the overall dataframe
             data_local = data_local.append(temp_data)
+    
+    # Replace fill-value with NaN
     data_local = data_local.replace(9999.0, nan)
+    
+    # Merge data_local (valid TS values) to data (all the needed TS values)
     data = pd.merge(data, data_local, how='left')
-    # moving average
+    
+    # Drop forecast lead time column
     keys = ['OTS', 'ENS', 'FRE']
     data_ma = data.copy()
-    data_ma = data_ma.drop('period', 1)
+    data_ma = data_ma.drop('lead', 1)
+    
+    # Moving average each TS group
     for key in keys:
-        data_ma[key] = data[key].rolling(min_periods=1, window=10, center=False).mean() # pandas moving window method
-    # Normalization
+        # pandas moving window method
+        data_ma[key] = data[key].rolling(min_periods=1, window=10, center=False).mean()
+        
+    # TS normalization
     SUM = data_ma.sum(axis=1)
     for key in keys:
         data_ma[key] = data_ma[key].div(SUM)
+    
+    # data_ma: TS as a pandas frame, flag of success
     return data_ma, True
 
 def get_weighted_ensemble(cmpt1, W1, cmpt2, W2, cmpt3, W3):
--- libs/utility.py
+++ libs/utility.py
@@ -13,10 +13,32 @@ def subtrack_precip_lev(data):
     ##    [25, 50)
     ##    [50, inf)
     data1 = copy(data)
-    data1[data1<25]=0.0
-    data1[data1>=50]=0.0
+    data1[data1<25] = 0.0
+    data1[data1>=50] = 0.0
+    
     data2 = copy(data)
-    data2[data2<50]=0.0
+    data2[data2<50] = 0.0
+    
     data3 = copy(data)
-    data3[data3>=25]=0.0
+    data3[data3>=25] = 0.0
+    
     return data3, data1, data2
+
+def subtrack_precip_lev_heavy(data, heavy_thres):
+    # discretizing precipitation by thresholds:
+
+    data1 = copy(data)
+    data1[data1<25] = 0.0
+    data1[data1>=50] = 0.0
+    
+    data2 = copy(data)
+    data2[data2<50] = 0.0
+    data2[data1>=heavy_thres] = 0.0
+    
+    data3 = copy(data)
+    data3[data3>=25] = 0.0
+    
+    data4 = copy(data)
+    data4[data4<heavy_thres]=0.0
+    
+    return data3, data1, data2, data4
